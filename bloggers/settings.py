9ce3a58459d386488af9e5c66f60121581d06da8
Satvik <satvik.vash@gmail.com>
2022-09-06 21:28:26 +0530

    1.0

--- bloggers/settings.py
+++ bloggers/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['https://svbloggers.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['svbloggers.herokuapp.com','127.0.0.1']
 
 
 # Application definition
