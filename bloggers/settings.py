27f6f33c8a17950fa0617d1b802de70715a155ad
Satvik <satvik.vash@gmail.com>
2022-09-06 21:07:08 +0530

    1.0

--- bloggers/settings.py
+++ bloggers/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://svbloggers.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
--- bloggers/wsgi.py
+++ bloggers/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for bloggers project.
+WSGI config for portfolio project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bloggers.settings')
+os.environ["DJANGO_SETTINGS_MODULE"] = "portfolio.settings"
+
 
 application = get_wsgi_application()
+
